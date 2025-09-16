7909cc8b937a2d0af83a8657ecb7d602b6ac2a79
meiagarzon <meia-symone.garzon@mohawkcollege.ca>
2025-09-16 19:15:57 -0400

    Followed professor's methods

--- deconstruction/Program.cs
+++ deconstruction/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//GET PLAYER NAME & REMEMBER
+Console.WriteLine("What is your name?");
+string playerName = Console.ReadLine();
+int swordCost = 20;
+
+//TELL PLAYER ABOUT BRONZE SWORD
+Console.WriteLine($"You can purchase our bronze sword for {swordCost} gold.");
+
+//ASK PLAYER FOR SUM OF HELD GOLD
+Console.WriteLine("How much gold do you have with you?");
+Console.WriteLine("Enter number:");
+int playerGold = int.Parse(Console.ReadLine());
+
+//BUY ITEM IF ABLE, DEDUCT COST FROM GOLD
+if (playerGold >= swordCost)
+{
+    playerGold -= swordCost;
+    Console.WriteLine($"Remaining gold: {playerGold}");
+}
+
+else
+{
+    Console.WriteLine("You have an insufficient amount of gold!");
+}
